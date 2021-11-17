15b0303be82ddd5c325339a6706e9b6d542d2487
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-11-17 13:57:57 +0100

    Refactor

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 (function guessingGame() {
-//create a random (whole) number between 1-15
-    function randomNmb(min, max) { //min = 1(incl), max = 16(excl)
+    //create a random (whole) number between 1-15
+    function randomNmb(max) { //min = 1(incl), max = 16(excl)
         return Math.floor(Math.random() * max) + 1;// round to whole numbers, min = 1
     }
-//console.log(randomNmb()) //Nan, because function
-
-// if (randomNmb !== isNan) {
-//     prompt("Please enter a valid number.");
-// }
 
     const classNmb = 15;
     let secretNmb = parseInt(randomNmb(classNmb));
-    console.log(secretNmb)
+    // console.log(secretNmb)
 
-// compare random number to userGuess when btn click
-// register userGuess between 1-15 (after btn click => in function)
-// display different messages
+    // register userGuess between 1-15 (after btn click => in function)
     document.getElementById("buttonClick").addEventListener("click", function () {
         let userGuess = parseInt(document.getElementById("guessedNum").value)
-
+        // compare random number to userGuess when btn click
+        // display corresponding messages
         if (userGuess === secretNmb) {
             alert(`Awesome! You number ${userGuess} was correct. You can be named many things, hungry not being one of them.`);
         } else if (userGuess + 1 === secretNmb || userGuess - 1 === secretNmb) {
